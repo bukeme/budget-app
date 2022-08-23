0913cdc42fb92d7f9df7221b514e7ad93d0a4cf7
bukeme <basseyukemme@gmail.com>
2022-08-23 05:51:42 +0100

    final project

--- index.js
+++ index.js
@@ -1,18 +1,128 @@
+const incomeHeaderAmount = document.querySelector('.income-header p:last-child')
+const outcomeHeaderAmount = document.querySelector('.outcome-header p:last-child')
+const balanceAmount = document.querySelector('.balance').children
+
 const tabs = document.querySelectorAll('.budget-dashboard h2')
-const incomeTab = document.querySelector('.income h2')
-const expenseTab = document.querySelector('.expense h2')
-const allTab = document.querySelector('.income h2')
-const incomeTitle = document.querySelector('.income input[type=text]')
-const incomeAmount = document.querySelector('.income input[type=number]')
-const addIncome = document.querySelector('.income input[type=submit]')
-const expenseTitle = document.querySelector('.expense input[type=text]')
-const expenseAmount = document.querySelector('.expense input[type=number]')
-const addExpense = document.querySelector('.expense input[type=submit]')
+const incomeTab = document.querySelector('.income-container h2')
+const expenseTab = document.querySelector('.expense-container h2')
+const allTab = document.querySelector('.income-container h2')
+const incomeList = document.querySelector('.income-container ul')
+const expenseList = document.querySelector('.expense-container ul')
+const allList = document.querySelector('.all ul')
+const listsEl = document.querySelectorAll('.list')
+const incomeTitle = document.querySelector('.income-container input[type=text]')
+const incomeAmount = document.querySelector('.income-container input[type=number]')
+const addIncome = document.querySelector('.income-container input[type=submit]')
+const expenseTitle = document.querySelector('.expense-container input[type=text]')
+const expenseAmount = document.querySelector('.expense-container input[type=number]')
+const addExpense = document.querySelector('.expense-container input[type=submit]')
+const chart = document.querySelector('.chart')
 
-let entryList = []
+
+const canvas = document.createElement('canvas')
+canvas.width = 120
+canvas.height = 120
+chart.appendChild(canvas)
+let ctx = canvas.getContext('2d')
+ctx.lineWidth = 10
+
+let incomeBalance = 0,
+    outcomeBalance = 0,
+    balance = 0,
+    deleteIcon = 'fa fa-trash',
+    editIcon = 'fas fa-edit'
+let entryList = JSON.parse(localStorage.getItem('budget-entries')) ? JSON.parse(localStorage.getItem('budget-entries')) : []
+updateUI()
 
 
 
+function drawCircle(color, ratio, anticlockwise) {
+    ctx.beginPath()
+    ctx.strokeStyle = color
+    ctx.arc(60, 60, 50, 0, Math.PI * ratio * 2, anticlockwise)
+    ctx.stroke()
+
+}
+
+
+
+
+function deleteItem(e) {
+    if (e.target.className === deleteIcon) {
+        console.log(e.target.parentElement.parentElement)
+        let itemId = e.target.parentElement.parentElement.attributes.id.value
+        entryList.splice(itemId, 1)
+        updateUI()
+    }
+}
+
+function editItem(e) {
+    if (e.target.className === editIcon) {
+        const listItem = e.target.parentElement.parentElement
+        if (listItem.className === 'income') {
+            incomeTitle.value = entryList[listItem.attributes.id.value].title
+            incomeAmount.value = entryList[listItem.attributes.id.value].amount
+            entryList.splice(listItem.attributes.id.value, 1)
+        } else if (listItem.className === 'expense') {
+            expenseTitle.value = entryList[listItem.attributes.id.value].title
+            expenseAmount.value = entryList[listItem.attributes.id.value].amount
+            entryList.splice(listItem.attributes.id.value, 1)
+        }
+        console.log(entryList)
+        updateUI()
+    }
+}
+
+function clearInputs(inputs) {
+    inputs.forEach(function(input) {
+        input.value = ''
+    })
+}
+
+function clearList() {
+    listsEl.forEach(function(list) {
+        list.innerHTML = ''
+    })
+}
+
+function updateHeader() {
+    incomeHeaderAmount.innerHTML = '$' + incomeBalance
+    outcomeHeaderAmount.innerHTML = '$' + outcomeBalance
+    balanceAmount[0].innerHTML = incomeBalance < outcomeBalance ? '-$' : '$'
+    balanceAmount[1].innerHTML = balance
+}
+
+function addEntry(list, type, title, amount, id) {
+    let listItem = `<li class="${type}" id="${id}">
+                        ${title}: $${amount}
+                        <div>
+                            <i class="fas fa-edit"></i>
+                            <i class="fa fa-trash"></i>
+                        </div>
+                    </li>`
+    list.insertAdjacentHTML('afterbegin', listItem)
+}
+
+function updateUI() {
+    clearList()
+    incomeBalance = 0, outcomeBalance = 0, balance = 0
+    entryList.forEach(function(entry, index) {
+        if (entry.type === 'income') {
+            addEntry(incomeList, entry.type, entry.title, entry.amount, index)
+            incomeBalance += parseFloat(entry.amount)
+        } else {
+            addEntry(expenseList, entry.type, entry.title, entry.amount, index)
+            outcomeBalance += parseFloat(entry.amount)
+        }
+        addEntry(allList, entry.type, entry.title, entry.amount, index)
+    })
+    balance = Math.abs(incomeBalance - outcomeBalance)
+    updateHeader()
+    let ratio = incomeBalance / (incomeBalance + outcomeBalance)
+    drawCircle('lawngreen', -ratio, true)
+    drawCircle('tomato', 1 - ratio, false)
+    localStorage.setItem('budget-entries', JSON.stringify(entryList))
+}
 
 function budget(type, title, amount) {
     if (!title || !amount) return
@@ -22,7 +132,8 @@ function budget(type, title, amount) {
         amount: amount
     }
     entryList.push(entry)
-    console.log(entryList)
+    updateUI()
+    clearInputs([incomeTitle, incomeAmount, expenseTitle, expenseAmount])
 }
 
 
@@ -37,8 +148,23 @@ tabs.forEach(function(tab) {
 
 addIncome.addEventListener('click', function() {
     budget('income', incomeTitle.value, incomeAmount.value)
+
 })
 
 addExpense.addEventListener('click', function() {
     budget('expense', expenseTitle.value, expenseAmount.value)
+})
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        budget('income', incomeTitle.value, incomeAmount.value)
+        budget('expense', expenseTitle.value, expenseAmount.value)
+    }
+})
+
+listsEl.forEach(function(list) {
+    list.addEventListener('click', function(e) {
+        deleteItem(e)
+        editItem(e)
+    })
 })
